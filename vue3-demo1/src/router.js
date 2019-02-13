b82f91637163b9417fb6c2589c10b114bcd2e11c
zhaoxianqiang <24930662@qq.com>
2019-02-13 10:27:43 +0800

    update

--- vue3-demo1/src/router.js
+++ vue3-demo1/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import product from './views/product.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+     {
+      path: '/product',
+      name: 'product',
+      component: product
     },
     {
       path: '/about',
